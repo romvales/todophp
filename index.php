ff01abb9c2a775cd971b4835f664574e8ad55cfa
Rom Vales Villanueva <romdevmod@gmail.com>
2024-08-25 09:24:54 +0800

    Latest commit

--- index.php
+++ index.php
@@ -20,14 +20,14 @@ define('HTTP_SERVICE_UNAVAILABLE', 503);
 $router = new Router();
 
 $router->options('/(api|auth)/.*', function() {
-  header('Access-Control-Allow-Origin: http://localhost:5173');
+  header('Access-Control-Allow-Origin: http://localhost:5173, https://todophp-git-main-rom-vales-projects.vercel.app');
   header('Access-Control-Allow-Credentials: true');
   header('Access-Control-Allow-Methods: GET,HEAD,PUT,POST,DELETE');
   header('Access-Control-Allow-Headers: Content-Type, Accept');
 });
 
 $router->before('GET|POST|PUT|DELETE', '/(api|auth).*', function() {
-  header('Access-Control-Allow-Origin: http://localhost:5173');
+  header('Access-Control-Allow-Origin: http://localhost:5173, https://todophp-git-main-rom-vales-projects.vercel.app');
   header('Access-Control-Allow-Credentials: true');
   header('Access-Control-Allow-Methods: GET,HEAD,PUT,POST,DELETE');
   header('Access-Control-Allow-Headers: Content-Type, Accept');
